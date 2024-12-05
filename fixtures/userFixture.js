2f14b16224d492f3ccdac0858a95e852298af104
Zorgos <pierre-louis.sans@edu.devinci.fr>
2024-12-05 14:34:37 +0100

    feat: offre fixture :sparkles:

--- fixtures/userFixture.js
+++ fixtures/userFixture.js
@@ -6,15 +6,23 @@ dotenv.config();
 const pw = await hashPassword(process.env.FIXTURE_PW);
 const prisma = new PrismaClient();
 
-await prisma.user.createMany({
-    data: [
-       { name: "Anna", password: pw, role: "SUPER_ADMIN" },
-       { name: "Tom", password: pw, role: "ADMIN" },
-       { name: "Bob", password: pw, role: "ADMIN" },
-       { name: "Lucie", password: pw, role: "CLASSIC" },
-       { name: "John", password: pw, role: "CLASSIC" },
-       { name: "Rob", password: pw, role: "CLASSIC" },
-       { name: "Leila", password: pw, role: "CLASSIC" },
-    ],
-    skipDuplicates: true, 
-});
+async function main() {
+    await prisma.user.createMany({
+        data: [
+           { name: "Anna", password: pw, role: "SUPER_ADMIN" },
+           { name: "Tom", password: pw, role: "ADMIN" },
+           { name: "Bob", password: pw, role: "ADMIN" },
+           { name: "Lucie", password: pw, role: "CLASSIC" },
+           { name: "John", password: pw, role: "CLASSIC" },
+           { name: "Rob", password: pw, role: "CLASSIC" },
+           { name: "Leila", password: pw, role: "CLASSIC" },
+        ],
+        skipDuplicates: true, 
+    });
+};
+
+main().then(() => {
+    console.log("Users created successfully");
+}).catch((e) => {
+    console.error("Error creating users", e);
+})
