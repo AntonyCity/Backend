8c31a4710c066924d44d5be04209c225b20ea93c
Zorgos <pierre-louis.sans@edu.devinci.fr>
2024-12-05 12:18:54 +0100

    feat: login fonctionnal :sparkles:

--- fixtures/userFixture.js
+++ fixtures/userFixture.js
@@ -1,5 +1,5 @@
 import { hashPassword } from '../utils/bcrypt.js';
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
